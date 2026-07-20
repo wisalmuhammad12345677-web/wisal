21d6a1191b503f7febe251bc66cb1812ba804983
Wisal Muhammad <wisalmuhammad12345677@gmail.com>
2026-07-21 01:21:18 +0500

    Add files via upload
    work on javascript object and loop

---
+++ ha.js
@@ -0,0 +1,78 @@
+  /*  let students = {
+    name:"wisal",
+    regnumber:"23623",
+    department:"CSS",
+}
+console.log(students)
+alert("wisal")*/
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b =", a+b)
+// console.log("a - b =", a-b)
+// console.log("a / b =", a/b)
+// console.log("a * b =", a*b)
+// console.log("a % b =", a%b)
+// console.log("a ** b =", a**b)
+
+
+// let num = 30;
+
+
+// if (num>40){
+//    console.log("you can Work");
+// }else{
+//     console.log("notwote")
+// }   
+
+
+//   let num = 30;
+//   if(num % 2 === 0){
+//     console.log(num,"is even")
+//   }else{
+//     console.log(num,"is odd")
+//   }
+
+
+//   alart("wisal");
+
+
+// let num = prompt("enter number")
+// if(num % 5 ===0){
+//     console.log(num, "is multiple")
+// }else{
+//     console.log(num,"is not multiple")
+    
+// }
+
+
+// let scoure =  95;
+// let grade;
+// if (scoure >=90 && scoure<80){
+// grade = "A";
+// }else if(scoure >=80 && scoure<70){
+//     grade ="B";
+// }else if(scoure >=70 && scoure<60){
+//     grade ="C";
+// }
+// else if(scoure >=60 && scoure<50){
+//     grade ="D";
+// }
+// console.log("acourding to your scoure is ",grade);
+// let sum = 0;
+// let n = 7;
+// for(let i = 1; i <= n; i++){
+//    sum = sum + 1;
+  
+// }
+// console.log("sum is =", sum)
+// console.log("the loop is ended")
+
+let gamnum = 25;
+let usernum = prompt("Guse you enter the number")
+while(usernum != gamnum){
+  usernum =prompt("gust you wrong number Plese try again" )
+}
+console.log("congratulation, you enter the write number")
+
